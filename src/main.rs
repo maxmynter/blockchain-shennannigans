cb22670bd5b51c7e3f1c8c33c0ee4cceaaf725be
Max Mynter <maxmynter@me.com>
2025-02-25 17:58:35 -0500

    Add port adding to main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,23 @@ mod utils;
 
 use api::server;
 use blockchain::{Chain, ProofOfWork};
-use chrono::Utc;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let mut chain = Chain::new(ProofOfWork::new(4));
+    if let Some(port) = std::env::args().nth(1) {
+        let port = port.parse::<u16>().expect("Invalid port number");
+        let mut chain = Chain::new(ProofOfWork::new(4));
+        if port != 8080 {
+            chain.register_node(format!("http://127.0.0.1:8080"));
+        }
+        if port != 8081 {
+            chain.register_node(format!("http://127.0.0.1:8081"));
+        }
+        println!("Starting node on port {}", port);
 
-    chain.register_node("http://127.0.0.1:8081".to_string());
-
-    let block = chain.new_block("Ad Astra".to_string(), Utc::now().timestamp());
-
-    if let Err(e) = api::client::broadcast_block(&chain, &block.clone()).await {
-        eprintln!("Failed to broadcast block {}", e);
+        server::run_server(chain, &format!("127.0.0.1:{}", port)).await
+    } else {
+        println!("Please pass port number");
+        Ok(())
     }
-
-    server::run_server(chain, "127.0.0.1:8080").await?;
-    Ok(())
 }
